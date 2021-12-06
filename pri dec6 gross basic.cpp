2ba6ecb22e86657bb31488c194671c7d7b79699e
prishita <95331398+VU21CSEN0100122@users.noreply.github.com>
2021-12-07 00:07:15 +0530

    Add files via upload

---
+++ pri dec6 gross basic.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	float basic,hra,da,gross;
+	printf("enter basic salary of an employee:");
+	scanf("%f",&basic);
+	if(basic<=10000)
+	{
+		da=basic*0.8;
+		hra=basic*0.2;
+	}
+	else if(basic<=20000)
+	{
+		da=basic*0.9;
+		hra=basic*0.25;
+	}
+	else
+	{
+		da=basic*0.95;
+		hra=basic*0.3;
+	}
+	gross=basic+hra+da;
+	printf("GROSS SALARY OF EMPLOYEE=%.2f",gross);
+}
